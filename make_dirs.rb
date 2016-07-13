7e9c291dff0d1b224b37a7531c64116392629915
xraystyle <ben@Bens-Macbook-Pro.local>
2016-07-13 09:16:04 -0700

    Clean up whitespace and formatting.

--- make_dirs.rb
+++ make_dirs.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby -w
 
-require 'pathname'	
+require 'pathname'  
 
 Dir.mkdir("testing_dir")
 
@@ -14,65 +14,43 @@ Dir.chdir("testing_dir")
 @dir_count = 0
 
 trap ("INT") {
-
-	if @colon_count > 0
-
-		puts
-		puts
-		puts "#{@colon_count} directories with colons created."
-		puts "#{@dir_count} total directories were created."
-
-	end
-
-	exit!
-
+  if @colon_count > 0
+    puts
+    puts
+    puts "#{@colon_count} directories with colons created."
+    puts "#{@dir_count} total directories were created."
+  end
+  exit!
 }
 
 
 def make_subdirs(path)
 
-	Dir.chdir(path)
+  Dir.chdir(path)
+  (0..10).each do |i|
 
-	(0..10).each do |i|
+    if rand(0..99) >= 75
+      rand_dir = Pathname.new(Dir.pwd + "/" + rand(100..9999).to_s + ":" + "abcabc")
+      @colon_count += 1
+    else
+      rand_dir = Pathname.new(Dir.pwd + "/" + rand(100..9999).to_s)
+    end 
 
-		if rand(0..99) >= 75
-
-			rand_dir = Pathname.new(Dir.pwd + "/" + rand(100..9999).to_s + ":" + "abcabc")
-			@colon_count += 1
-
-		else
-
-			rand_dir = Pathname.new(Dir.pwd + "/" + rand(100..9999).to_s)
-
-		end 
-		
-
-		puts "\t Making Dir #{rand_dir}"
-
-		Dir.mkdir(rand_dir) unless Dir.exists?(rand_dir)
-
-		@dir_count += 1
-
-		@dirs << rand_dir
-
-	end
-
-	# begin
-		@dirs.delete(path)
-	# end
-
+    puts "\t Making Dir #{rand_dir}"
+    Dir.mkdir(rand_dir) unless Dir.exists?(rand_dir)
+    @dir_count += 1
+    @dirs << rand_dir
+  end
+  
+    @dirs.delete(path)
 end
 
 
-while @dir_count <= 10000
-	
-	@dirs.each do |d|
-
-		make_subdirs(d)
-
-	end
-
+while @dir_count <= 10000  
+  @dirs.each do |d|
+    make_subdirs(d)
+  end
 end
 
 
-		
+    
--- rename.rb
+++ rename.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/ruby -w
 
 unless ARGV[0]
-
-	puts "Needs a directory as a command line argument."
-	exit!
-	
+  puts "Needs a directory as a command line argument."
+  exit!
 end
 
 Dir.chdir(ARGV[0])
@@ -19,12 +17,9 @@ require 'pathname'
 # Counter to keep track of the number of renamed entries.
 @renamed = 0
 
-
 # Fill it with pathnames from the given directory.
 Dir['*'].each do |f|
-
-	@dirs << Pathname.new(File.expand_path(f))
-
+  @dirs << Pathname.new(File.expand_path(f))
 end
 
 
@@ -32,70 +27,57 @@ end
 # Method that renames a path with colons to the same path, but replacing
 # colons with hyphens.
 def rename_colons(path)
-
-	File.rename(File.expand_path(path), File.expand_path(path.to_s.gsub(":", "-")))
-	@renamed += 1
-
+  File.rename(File.expand_path(path), File.expand_path(path.to_s.gsub(":", "-")))
+  @renamed += 1
 end
 
 
 # Get all subdirectories of a directory, append them to @dirs.
 # This avoids the need for a recursive method to dig through directories.
 def get_subs(path)
-
-		path.children.each do |e|
-
-			next if e.to_s == "."
-			next if e.to_s == ".."
-			next if /.*\._.*/.match e.to_s
-
-			@dirs << Pathname.new(File.expand_path(e))
-
-		end
-
+    path.children.each do |e|
+      next if e.to_s == "."
+      next if e.to_s == ".."
+      next if /.*\._.*/.match e.to_s
+      @dirs << Pathname.new(File.expand_path(e))
+    end
 end
 
 puts "Working..."
 
 until @dirs.empty? do
-	
-	# Take a directory, look at it.
-	@dirs.each do |f|
+  
+  # Take a directory, look at it.
+  @dirs.each do |f|
 
-		# system 'clear'
-		# puts "Parsing #{f}..."
+    if /.*:+.*/.match(f.to_s)
 
-		if /.*:+.*/.match(f.to_s)
+      # If it has colons, rename it.
+      rename_colons(f)
 
-			# If it has colons, rename it.
-			rename_colons(f)
+      # Find all the entries one level below the dir we're working with,
+      # unless it's a file, keeping in mind that it's been renamed already.
+      # Make sure that everything is a Pathname instance instead of a string.
+      get_subs(Pathname.new(File.expand_path(f.to_s.gsub(":", "-")))) if File.directory?(File.expand_path(f.to_s.gsub(":", "-")))
 
-			# Find all the entries one level below the dir we're working with,
-			# unless it's a file, keeping in mind that it's been renamed already.
-			# Make sure that everything is a Pathname instance instead of a string.
-			get_subs(Pathname.new(File.expand_path(f.to_s.gsub(":", "-")))) if File.directory?(File.expand_path(f.to_s.gsub(":", "-")))
+      # Remove the entry from the @dirs array
+      
+      # .delete returns nil if 'f' isn't in the array. This
+      # substantially increases the speed of the script, as 
+      # opposed to checking if it's there via @dirs.include?(f) 
+      # first. .include? requires a brute-force check against 
+      # every other item in the array.
+      @dirs.delete(f) 
+      
+    else
+      # do the same as above, except without the renaming.
+      get_subs(f) if File.directory?(f)
 
-			# Remove the entry from the @dirs array
-			
-			# .delete returns nil if 'f' isn't in the array. This
-			# substantially increases the speed of the script, as 
-			# opposed to checking if it's there via @dirs.include?(f) 
-			# first. .include? requires a brute-force check against 
-			# every other item in the array.
-			@dirs.delete(f) 
-			
+      @dirs.delete(f) 
+      
+    end
 
-		else
-			# do the same as above, except without the renaming.
-			get_subs(f) if File.directory?(f)
-
-			
-			@dirs.delete(f) 
-			
-		end
-
-	end
-
+  end
 
 end
 puts
